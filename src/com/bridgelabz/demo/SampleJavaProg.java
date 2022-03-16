e6052269c71b93ec2cd0fa2b0d6a7b42ed8b7e6f
Mahesh Kande <maheshkande0707@gmail.com>
2022-03-16 15:51:47 +0530

    First Day

---
+++ src/com/bridgelabz/demo/SampleJavaProg.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.demo;
+
+public class SampleJavaProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Declaration
+		String newVariable;
+		
+		// initializing the value with declaration
+		System.out.println("Hello Everyone");
+		System.out.println("How r U");
+		System.out.println(" ");
+		System.out.println("Mahesh");
+		System.out.println("Hellooo");
+		
+		//assingning the Value
+		newVariable="Bridgelabz";
+	}
+
+}
